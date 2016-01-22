fd5b924902f8ed744ccb898ec0e68c2981440e69
Matt Rosanio <mmrosani@syr.edu>
2016-01-22 08:29:29 -0800

    add test and functionallity to replace any number divisible by 3 and 5 with 'pingpong'

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,12 @@
 function pingPong(countTo) {
   var numberArray = createNumberArray(countTo);
   for (var i = 0; i < numberArray.length; i++) {
-    if(numberArray[i]%3 === 0) {
+    if(numberArray[i]%3===0 && numberArray[i]%5===0) {
+      var firstPart = numberArray.slice(0, i);
+      var secondPart = numberArray.slice((i+1), numberArray.length);
+      firstPart.push('pingpong');
+      numberArray = firstPart.concat(secondPart);
+    } else if(numberArray[i]%3 === 0) {
       var firstPart = numberArray.slice(0, i);
       var secondPart = numberArray.slice((i+1), numberArray.length);
       firstPart.push('ping');
--- spec/specs.js
+++ spec/specs.js
@@ -11,4 +11,7 @@ describe("pingPong", function() {
   it('will replace any number which is divisible by five with the word "pong"', function() {
     expect(pingPong(10)).to.eql([1,2,'ping',4,'pong','ping',7,8,'ping','pong']);
   });
+  it('will replace any number which is divisible by both three and five with the word "pingpong"',function() {
+    expect(pingPong(17)).to.eql([1,2,'ping',4,'pong','ping',7,8,'ping', 'pong',11,'ping',13,14,'pingpong',16,17]);
+  });
 });
