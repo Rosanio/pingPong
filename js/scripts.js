7d44989691de881aba3ac9439ca79858a0d39de5
Matt Rosanio <mmrosani@syr.edu>
2016-01-22 08:03:24 -0800

    add a test and function to create an array of numbers counting up to a specified number

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+function createNumberArray(countTo) {
+  var array = [];
+  for(var i = 1; i <= countTo; i++) {
+    array.push(i);
+  }
+  return array;
+}
--- spec/specs.js
+++ spec/specs.js
@@ -1,1 +1,5 @@
-
+describe("createNumberArray", function() {
+  it("will generate an array of numbers up to number which user inputs", function() {
+    expect(createNumberArray(15)).to.eql([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]);
+  });
+});
