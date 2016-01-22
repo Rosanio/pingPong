f21940f816ca0ef1348c4396c7a44e17da546b34
Matt Rosanio <mmrosani@syr.edu>
2016-01-22 08:51:39 -0800

    refactor pingpong and remove createNumberArray function and test

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,18 @@
 function pingPong(countTo) {
-  var numberArray = createNumberArray(countTo);
-  for (var i = 0; i < numberArray.length; i++) {
-    if(numberArray[i]%3===0 && numberArray[i]%5===0) {
-      var firstPart = numberArray.slice(0, i);
-      var secondPart = numberArray.slice((i+1), numberArray.length);
-      firstPart.push('pingpong');
-      numberArray = firstPart.concat(secondPart);
-    } else if(numberArray[i]%3 === 0) {
-      var firstPart = numberArray.slice(0, i);
-      var secondPart = numberArray.slice((i+1), numberArray.length);
-      firstPart.push('ping');
-      numberArray = firstPart.concat(secondPart);
-    } else if (numberArray[i]%5 === 0) {
-      var firstPart = numberArray.slice(0, i);
-      var secondPart = numberArray.slice((i+1), numberArray.length);
-      firstPart.push('pong');
-      numberArray = firstPart.concat(secondPart);
-    }
-  }
-  return numberArray;
-}
-
-function createNumberArray(countTo) {
   var numberArray = [];
   for(var i = 1; i <= countTo; i++) {
-    numberArray.push(i);
+    var output = "";
+    if (i%3 === 0) {
+      output += 'ping';
+    }
+    if (i%5 === 0) {
+      output += 'pong';
+    }
+    if (output.length > 0) {
+      numberArray.push(output);
+    } else {
+      numberArray.push(i);
+    }
   }
   return numberArray;
 }
